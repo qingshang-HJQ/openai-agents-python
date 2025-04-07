57e63efbeb4de9ecf4b79e1aab3000c24069acb7
huangjianqiao <huangjianqiao@crdigital.com.cn>
2025-04-07 18:53:38 +0800

    tool

--- customer_service/agent/agent.py
+++ customer_service/agent/agent.py
@@ -1,6 +1,14 @@
-from agents import Agent
+from agents import Agent,Runner
+
+from customer_service.agent.llm import llm_model
+from customer_service.prompt import intent_recognizer_agent_pormpt
+from customer_service.tools.order_tool import logistics_query, mock_refund_apply
+from agents.run_context import RunContextWrapper
 
 
+class DummyContext:
+    def __init__(self):
+        self.data = "something"
 class RouteAgent(Agent):
     def __init__(self, name: str, **kwargs):
         super().__init__(name, **kwargs)
@@ -8,9 +16,25 @@ class RouteAgent(Agent):
     def route_agent(self):
         return Agent(
             name="用户意图分析师",
-            description=intent_recognizer_agent_pormpt,
-            llm=llm_model(),
+            instructions=intent_recognizer_agent_pormpt,
+            model=llm_model(),
+            tools=[
+                logistics_query,
+                mock_refund_apply
+            ]
         )
 
-    async  def plan(self, input: str, history: list):
-        intent = detect_user_intent(input)
+
+    async def plan(self, input: str, history: list):
+        result = await Runner.run(self.route_agent(), input)
+        intent_result = result.final_output
+        intent = intent_result.get("intent")
+        slots = intent_result.get("slots", {})
+
+        if intent == "查询物流":
+            tool = logistics_query
+            output = await tool.on_invoke_tool(ctx_wrapper(), "")
+        elif intent == "退货申请":
+            return self.run_tool("refund_request", intent_result)
+        else:
+            return "抱歉，我暂时无法理解您的请求。可以重新描述一下吗？"
---
+++ customer_service/tools/order_tool.py
@@ -0,0 +1,22 @@
+from agents import function_tool
+
+
+@function_tool
+async def logistics_query(intent_result: dict) -> str:
+    """
+    调用查询物流信息的接口
+    """
+    if isinstance(intent_result, dict):
+        order_id = intent_result.get("order_id")
+        return f"订单{order_id}的物流信息是：。。。。"
+    return "intent_result must be dict"
+
+@function_tool
+async def mock_refund_apply(intent_result: dict) -> str:
+    """
+    退货
+    """
+    if isinstance(intent_result, dict):
+        order_id = intent_result.get("order_id")
+        return f"订单{order_id}的退款申请已提交，请耐心等待。"
+    return "intent_result must be dict"
